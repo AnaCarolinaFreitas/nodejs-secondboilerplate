93a5b19910f9c22ccac82232514329e2ad15290c
Ana Carolina <anacarolinagarciafreitas@gmail.com>
2024-10-31 15:03:30 -0300

    feat: create user class

---
+++ src/models/users/User.js
@@ -0,0 +1,14 @@
+class User {
+  constructor( name, email, password) {
+    this.id = this.generateid();
+    this.name = name;   
+    this.email = email;
+    this.password = password;
+  }
+
+  generateid() {
+    return Math.floor(Math.random() * 999 + 1);
+  }
+}
+
+export default User;    

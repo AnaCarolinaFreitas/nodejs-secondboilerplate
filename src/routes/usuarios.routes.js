478ebadf7b89acd09303468d6a72b9c4be79fe28
Ana Carolina <anacarolinagarciafreitas@gmail.com>
2024-10-31 17:10:42 -0300

    feat:create update user by id method

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -49,11 +49,26 @@ usuariosRoutes.get("/:id", (req, res) => {
 });
 
 usuariosRoutes.put("/:id", (req, res) => {
-    
+    const { id } = req.params;
+    const { name, email, password } = req.body;
+
+    const user = userslist.updateUser(id, name, email, password);
+
+    if (!user) {
+      return res.status(404).json({
+        message: `Usuário com id ${id} não encontrado`,
+      });
+    }
+
+    return res.status(200).json({
+      message: `Usuário com id ${id} atualizado`,
+      user,
+    });
 });
 
 usuariosRoutes.delete("/:id", (req, res) => {
     
 });
+
 
 export default usuariosRoutes;
